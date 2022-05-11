be699ea7a76790c0096d3e368a00a7111b87e7dd
Diego Aguilera Martín <91873599+DiegoAguileraMartin@users.noreply.github.com>
2022-05-11 20:53:53 +0200

    Add files via upload

--- bbdd/lolcrud.sql
+++ bbdd/lolcrud.sql
@@ -1,8 +1,9 @@
 CREATE TABLE usuario
 (
-  CodUsu INT NOT NULL,
+  CodUsu INT NOT NULL AUTO_INCREMENT,
   AliasUsu VARCHAR(50) NOT NULL,
   Contrasena VARCHAR(30) NOT NULL,
+  Administrador CHAR(1) CHECK(Administrador ='s'),
   PRIMARY KEY (CodUsu)
 );
 
@@ -13,7 +14,8 @@ CREATE TABLE listado_campeon
   LineaCamp VARCHAR(10) NOT NULL,
   RolHabCamp VARCHAR(15) NOT NULL,
   DescCamp VARCHAR(250) NOT NULL,
-  /*CodUsu INT NOT NULL*/
-  PRIMARY KEY (CodCamp)
-  /*FOREIGN KEY (CodUsu) REFERENCES usuario(CodUsu)*/
+  CodUsu INT NOT NULL,
+  PRIMARY KEY (CodCamp),
+  FOREIGN KEY (CodUsu) REFERENCES usuario(CodUsu)
+      ON UPDATE CASCADE ON DELETE CASCADE
 );
